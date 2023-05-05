391fa1ef325b73fc4d06a813ee682acb5d332e2e
Brent Barbachem <barbacbd@dukes.jmu.edu>
2023-05-05 15:37:49 -0400

    Added AWS scripts for creating and destroying resources needed for custom security group cards.

---
+++ installer/aws/custom_security_groups/CreateCustomSecurityGroup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+export AWS_SHARED_CREDENTIALS_FILE="/home/${USER}/.aws/credentials"
+EXPIRATION_DATE=$(date -d '4 hours' --iso=minutes --utc)
+TAGS="Key=expirationDate,Value=${EXPIRATION_DATE}"
+REGION="us-east-1"
+NAMESPACE="bbarbach-test"
+SHARED_DIR=/tmp
+ZONES_COUNT=3
+
+if [ ! -f "01_vpc.yaml" ]; then
+    wget https://raw.githubusercontent.com/openshift/installer/master/upi/aws/cloudformation/01_vpc.yaml
+fi
+
+STACK_NAME="${NAMESPACE}-vpc"
+aws --region "${REGION}" cloudformation create-stack \
+  --stack-name "${STACK_NAME}" \
+  --template-body "$(cat 01_vpc.yaml)" \
+  --tags "${TAGS}" \
+  --parameters "ParameterKey=AvailabilityZoneCount,ParameterValue=${ZONES_COUNT}" &
+
+wait "$!"
+echo "Created stack"
+
+aws --region "${REGION}" cloudformation wait stack-create-complete --stack-name "${STACK_NAME}" &
+wait "$!"
+echo "Waited for stack"
+
+
+# save stack information to SHARED_DIR for deprovision step
+echo "${STACK_NAME}" > "${SHARED_DIR}/vpc_stack_name"
+
+# save vpc stack output
+echo "describing stack"
+aws --region "${REGION}" cloudformation describe-stacks --stack-name --output json "${STACK_NAME}" > "${SHARED_DIR}/vpc_stack_output"
+echo "finished describing stack"
+
+# save vpc id
+# e.g.
+#   vpc-01739b6510a152d44
+VpcId=$(jq -r '.Stacks[].Outputs[] | select(.OutputKey=="VpcId") | .OutputValue' "${SHARED_DIR}/vpc_stack_output")
+echo "$VpcId" > "${SHARED_DIR}/vpc_id"
+echo "VpcId: ${VpcId}"
+
+# all subnets
+# ['subnet-pub1', 'subnet-pub2', 'subnet-priv1', 'subnet-priv2']
+AllSubnetsIds="$(jq -c '[.Stacks[].Outputs[] | select(.OutputKey | endswith("SubnetIds")).OutputValue | split(",")[]]' "${SHARED_DIR}/vpc_stack_output" | sed "s/\"/'/g")"
+echo "$AllSubnetsIds" > "${SHARED_DIR}/subnet_ids"
+
+# save public subnets ids
+# ['subnet-pub1', 'subnet-pub2']
+PublicSubnetIds="$(jq -c '[.Stacks[].Outputs[] | select(.OutputKey=="PublicSubnetIds") | .OutputValue | split(",")[]]' "${SHARED_DIR}/vpc_stack_output" | sed "s/\"/'/g")"
+echo "$PublicSubnetIds" > "${SHARED_DIR}/public_subnet_ids"
+echo "PublicSubnetIds: ${PublicSubnetIds}"
+
+# save private subnets ids
+# ['subnet-priv1', 'subnet-priv2']
+PrivateSubnetIds="$(jq -c '[.Stacks[].Outputs[] | select(.OutputKey=="PrivateSubnetIds") | .OutputValue | split(",")[]]' "${SHARED_DIR}/vpc_stack_output" | sed "s/\"/'/g")"
+echo "$PrivateSubnetIds" > "${SHARED_DIR}/private_subnet_ids"
+echo "PrivateSubnetIds: ${PrivateSubnetIds}"
+
+# save AZs
+#  ["us-east-2a","us-east-2b"]
+all_ids=$(jq -c '[.Stacks[].Outputs[] | select(.OutputKey | endswith("SubnetIds")).OutputValue | split(",")[]]' "${SHARED_DIR}/vpc_stack_output" | jq -r '. | join(" ")')
+AvailabilityZones=$(aws ec2 describe-subnets --region "${REGION}" --subnet-ids ${all_ids} --output json | jq -c '[.Subnets[].AvailabilityZone] | unique | sort')
+echo "$AvailabilityZones" > "${SHARED_DIR}/availability_zones"
+echo "AvailabilityZones: ${AvailabilityZones}"
+
+
+
---
+++ installer/aws/custom_security_groups/DeleteCustomSecurityGroup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+trap 'CHILDREN=$(jobs -p); if test -n "${CHILDREN}"; then kill ${CHILDREN} && wait; fi' TERM
+
+export AWS_SHARED_CREDENTIALS_FILE="/home/${USER}/.aws/credentials"
+REGION="us-east-1"
+SHARED_DIR=/tmp
+
+echo "Deleting AWS CloudFormation stacks"
+
+stack_list="${SHARED_DIR}/vpc_stack_name"
+if [ -e "${stack_list}" ]; then
+    for stack_name in `tac ${stack_list}`; do 
+        echo "Deleting stack ${stack_name} ..."
+        aws --region $REGION cloudformation delete-stack --stack-name "${stack_name}" &
+        wait "$!"
+        echo "Deleted stack ${stack_name}"
+
+        aws --region $REGION cloudformation wait stack-delete-complete --stack-name "${stack_name}" &
+        wait "$!"
+        echo "Waited for stack ${stack_name}"
+    done
+fi
+
+
+# remove files created during resource creation
+if [ -f "01_vpc.yaml" ]; then
+    rm 01_vpc.yaml
+fi
+
+rm -rf "${SHARED_DIR}/availability_zones"
+rm -rf "${SHARED_DIR}/private_subnet_ids"
+rm -rf "${SHARED_DIR}/public_subnet_ids"
+rm -rf "${SHARED_DIR}/subnet_ids"
+rm -rf "${SHARED_DIR}/vpc_id"
+rm -rf "${SHARED_DIR}/vpc_stack_name"
+rm -rf "${SHARED_DIR}/vpc_stack_output"
+
+exit 0
